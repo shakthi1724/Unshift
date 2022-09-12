db419721bb645e4e729305e10e312f624f670db4
siva <sivashakthiarun.c@gmail.com>
2022-09-12 17:02:43 +0530

    repush

---
+++ main.js
@@ -0,0 +1,7 @@
+var fabevy=[1,2,"Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+fabevy.unshift(17,18,"Subbu");
+console.log("Method:fabevy.unshift(17,18,'Subbu');");
+console.log("Change in array:",fabevy);
+console.log("Return:Length of array after unshift(); is "+fabevy.length);
